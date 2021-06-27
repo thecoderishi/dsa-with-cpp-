cb1bb284c49b3bb5b16a5fa071c98f7fc2c7cf52
thecoderishi <rishirverma512@gmail.com>
2021-06-27 21:05:29 +0530

    upto loop

---
+++ datatype.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a;
+    short int si;
+    long int li;
+    float b;
+    double c;
+    char d;
+    bool e;
+    cout << "size of int " << sizeof(a) << endl;
+    cout << "size of short int " << sizeof(si) << endl;
+    cout << "size of long int " << sizeof(li) << endl;
+    cout << "size of float " << sizeof(b) << endl;
+    cout << "size of double " << sizeof(c) << endl;
+    cout << "size of char " << sizeof(d) << endl;
+    cout << "size of bool " << sizeof(e) << endl;
+     return 0;
+}
